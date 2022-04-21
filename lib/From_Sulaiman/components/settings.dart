dad9d98f3f80d1a6ef6616b14c0377786b536de7
hashimniane <hassimiouniane@gmail.com>
2022-04-22 01:44:43 +0300

    trial commit added a new import package

--- lib/From_Sulaiman/components/settings.dart
+++ lib/From_Sulaiman/components/settings.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
 class Setting extends StatefulWidget {
   Setting({
